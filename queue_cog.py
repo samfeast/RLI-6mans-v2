7243d5984de20539eff609766a93fbee4740511f
Sam Feast <sfeast43@gmail.com>
2023-07-06 00:50:19 +0100

    handle queue with dictionary not list

--- queue_cog.py
+++ queue_cog.py
@@ -20,11 +20,6 @@ PREMIER_LOGS = config.PREMIER_LOGS
 CHAMPIONSHIP_LOGS = config.CHAMPIONSHIP_LOGS
 CASUAL_LOGS = config.CASUAL_LOGS
 
-elite_queue = []
-premier_queue = []
-championship_queue = []
-casual_queue = []
-
 
 def log_event(event):
     with open("logs/live_logs.csv", "a", newline="") as write_file:
@@ -32,7 +27,10 @@ def log_event(event):
         writer.writerow(event)
 
 
-class queue(commands.Cog):
+queue = {"elite": {}, "premier": {}, "championship": {}, "casual": {}}
+
+
+class queue_handler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -63,7 +61,7 @@ class queue(commands.Cog):
             ]
         )
         if interaction.channel.id == ELITE:
-            if interaction.user.id in elite_queue:
+            if interaction.user.id in queue["elite"]:
                 await interaction.response.send_message(
                     "You are already in the queue.", ephemeral=True
                 )
@@ -71,12 +69,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     interaction.user,
-                    elite_queue,
+                    "elite",
                     ELITE,
                     False,
                 )
         elif interaction.channel.id == PREMIER:
-            if interaction.user.id in premier_queue:
+            if interaction.user.id in queue["premier"]:
                 await interaction.response.send_message(
                     "You are already in the queue.", ephemeral=True
                 )
@@ -84,12 +82,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     interaction.user,
-                    premier_queue,
+                    "premier",
                     PREMIER,
                     False,
                 )
         elif interaction.channel.id == CHAMPIONSHIP:
-            if interaction.user.id in championship_queue:
+            if interaction.user.id in queue["championship"]:
                 await interaction.response.send_message(
                     "You are already in the queue.", ephemeral=True
                 )
@@ -97,12 +95,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     interaction.user,
-                    championship_queue,
+                    "championship",
                     CHAMPIONSHIP,
                     False,
                 )
         elif interaction.channel.id == CASUAL:
-            if interaction.user.id in casual_queue:
+            if interaction.user.id in queue["casual"]:
                 await interaction.response.send_message(
                     "You are already in the queue.", ephemeral=True
                 )
@@ -110,7 +108,7 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     interaction.user,
-                    casual_queue,
+                    "casual",
                     CASUAL,
                     False,
                 )
@@ -118,8 +116,6 @@ class queue(commands.Cog):
             await interaction.response.send_message(
                 "Queuing is not enabled in this channel.", ephemeral=True
             )
-
-        # Join queue
 
     @app_commands.command(description="Add a user to the queue.")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
@@ -133,7 +129,7 @@ class queue(commands.Cog):
             ]
         )
         if interaction.channel.id == ELITE_LOGS:
-            if user.id in elite_queue:
+            if user.id in queue["elite"]:
                 await interaction.response.send_message(
                     "User is already in the queue.", ephemeral=True
                 )
@@ -141,12 +137,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     user,
-                    elite_queue,
+                    "elite",
                     ELITE,
                     True,
                 )
         elif interaction.channel.id == PREMIER_LOGS:
-            if user.id in premier_queue:
+            if user.id in queue["premier"]:
                 await interaction.response.send_message(
                     "User is already in the queue.", ephemeral=True
                 )
@@ -154,12 +150,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     user,
-                    premier_queue,
+                    "premier",
                     PREMIER,
                     True,
                 )
         elif interaction.channel.id == CHAMPIONSHIP_LOGS:
-            if user.id in championship_queue:
+            if user.id in queue["championship"]:
                 await interaction.response.send_message(
                     "User is already in the queue.", ephemeral=True
                 )
@@ -167,12 +163,12 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     user,
-                    championship_queue,
+                    "championship",
                     CHAMPIONSHIP,
                     True,
                 )
         elif interaction.channel.id == CASUAL_LOGS:
-            if user.id in casual_queue:
+            if user.id in queue["casual"]:
                 await interaction.response.send_message(
                     "User is already in the queue.", ephemeral=True
                 )
@@ -180,7 +176,7 @@ class queue(commands.Cog):
                 await self.add_to_queue(
                     interaction,
                     user,
-                    casual_queue,
+                    "casual",
                     CASUAL,
                     True,
                 )
@@ -190,9 +186,9 @@ class queue(commands.Cog):
                 ephemeral=True,
             )
 
-    async def add_to_queue(self, interaction, user, queue, queue_channel_id, added):
+    async def add_to_queue(self, interaction, user, tier, queue_channel_id, added):
         queue_channel = self.bot.get_channel(queue_channel_id)
-        queue.append(user.id)
+        queue[tier][user.id] = round(time.time(), 2)
 
         if added:
             log_event(
@@ -200,7 +196,7 @@ class queue(commands.Cog):
                     round(time.time(), 2),
                     time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
                     "Queue",
-                    f"{user.name} [{user.id}] has been added to the {queue_channel.name[:-6]} queue ({len(queue)} in queue)",
+                    f"{user.name} [{user.id}] has been added to the {queue_channel.name[:-6]} queue ({len(queue[tier])} in queue)",
                 ]
             )
             embed = discord.Embed(
@@ -210,16 +206,18 @@ class queue(commands.Cog):
             )
             await interaction.response.send_message(embed=embed)
 
-            if len(queue) == 1:
+            if len(queue[tier]) == 1:
                 embed = discord.Embed(title="1 player is in the queue!")
                 embed.set_footer(
                     text="5 more needed!",
                     icon_url=f"https://cdn.discordapp.com/emojis/607596209254694913.png?v=1",
                 )
             else:
-                embed = discord.Embed(title=f"{len(queue)} players are in the queue!")
+                embed = discord.Embed(
+                    title=f"{len(queue[tier])} players are in the queue!"
+                )
                 embed.set_footer(
-                    text=f"{str(6-len(queue))} more needed!",
+                    text=f"{str(6-len(queue[tier]))} more needed!",
                     icon_url=f"https://cdn.discordapp.com/emojis/607596209254694913.png?v=1",
                 )
             embed.color = 0xFF8B00
@@ -231,20 +229,22 @@ class queue(commands.Cog):
                     round(time.time(), 2),
                     time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
                     "Queue",
-                    f"{user.name} [{user.id}] has joined the {queue_channel.name[:-6]} queue ({len(queue)} in queue)",
+                    f"{user.name} [{user.id}] has joined the {queue_channel.name[:-6]} queue ({len(queue[tier])} in queue)",
                 ]
             )
 
-            if len(queue) == 1:
+            if len(queue[tier]) == 1:
                 embed = discord.Embed(title="1 player is in the queue!")
                 embed.set_footer(
                     text="5 more needed!",
                     icon_url=f"https://cdn.discordapp.com/emojis/607596209254694913.png?v=1",
                 )
             else:
-                embed = discord.Embed(title=f"{len(queue)} players are in the queue!")
+                embed = discord.Embed(
+                    title=f"{len(queue[tier])} players are in the queue!"
+                )
                 embed.set_footer(
-                    text=f"{str(6-len(queue))} more needed!",
+                    text=f"{str(6-len(queue[tier]))} more needed!",
                     icon_url=f"https://cdn.discordapp.com/emojis/607596209254694913.png?v=1",
                 )
             embed.color = 0xFF8B00
@@ -253,4 +253,4 @@ class queue(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(queue(bot))
+    await bot.add_cog(queue_handler(bot))
