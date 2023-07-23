cea573bec0cfcadcbaa5200863d6d9f7934997b1
Sam Feast <sfeast43@gmail.com>
2023-07-23 21:00:18 +0100

    logging

--- reporting_cog.py
+++ reporting_cog.py
@@ -31,11 +31,27 @@ class reporting(commands.Cog):
     @app_commands.command(description="Ping the reporting cog.")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def ping_reporting(self, interaction: discord.Interaction):
+        log_event(
+            [
+                round(time.time(), 2),
+                time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
+                "Reporting",
+                f"'ping_reporting' used by {interaction.user.name} [{interaction.user.id}]",
+            ]
+        )
         await interaction.response.send_message("Pong!", ephemeral=True)
 
     @app_commands.command(description="Report a win.")
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     async def win(self, interaction: discord.Interaction, game_id: str):
+        log_event(
+            [
+                round(time.time(), 2),
+                time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
+                "Reporting",
+                f"'win' used by {interaction.user.name} [{interaction.user.id}] for {game_id.upper()}",
+            ]
+        )
         with open("json/game_log.json", "r") as read_file:
             game_log = json.load(read_file)
 
@@ -63,6 +79,14 @@ class reporting(commands.Cog):
             await self.report_win(interaction, game_id, winner, loser)
 
         else:
+            log_event(
+                [
+                    round(time.time(), 2),
+                    time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
+                    "Reporting",
+                    f"{game_id.upper()} does not exist",
+                ]
+            )
             await interaction.response.send_message(
                 f"{game_id} does not exist",
                 ephemeral=True,
@@ -104,6 +128,14 @@ class reporting(commands.Cog):
 
     async def report_win(self, interaction, game_id, winner, loser):
         if winner == None and loser == None:
+            log_event(
+                [
+                    round(time.time(), 2),
+                    time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
+                    "Reporting",
+                    f"{interaction.user.name} [{interaction.user.id}] does not have permission to report {game_id}",
+                ]
+            )
             await interaction.response.send_message(
                 f"You do not have permission to report {game_id.split('-')[0]}",
                 ephemeral=True,
@@ -126,6 +158,8 @@ class reporting(commands.Cog):
 
             game_log["complete"][game_id] = game_dict
             del game_log["live"][game_id]
+
+            game_log["stats_queue"].append(game_id)
 
             with open("json/game_log.json", "w") as write_file:
                 json.dump(game_log, write_file, indent=2)
@@ -163,23 +197,32 @@ class reporting(commands.Cog):
                         "wins": 0,
                         "losses": 1,
                         "points": -POINTS_FOR_LOSS,
-                        "elo": 1000 + elo_swing,
+                        "elo": 1000 - elo_swing,
                     }
 
             with open("json/player_data.json", "w") as write_file:
                 json.dump(player_data, write_file, indent=2)
+
+            log_event(
+                [
+                    round(time.time(), 2),
+                    time.strftime("%d-%m-%y %H:%M:%S", time.localtime()),
+                    "Reporting",
+                    f"{game_id} reported by {interaction.user.name} [{interaction.user.id}] with ELO swing {round(elo_swing,1)}",
+                ]
+            )
 
             embed = discord.Embed(
                 title=f"{game_dict['tier'].capitalize()} Game: {game_id.split('-')[0]}",
                 color=0x83FF00,
             )
             embed.add_field(
-                name="Winning Team",
+                name=f"Winning Team\t(+{round(elo_swing,1)} ELO)",
                 value=", ".join(player for player in mention_winning_players),
                 inline=False,
             )
             embed.add_field(
-                name="Losing Team",
+                name=f"Losing Team\t(-{round(elo_swing,1)} ELO)",
                 value=", ".join(player for player in mention_losing_players),
                 inline=False,
             )
